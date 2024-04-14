f631953abbc9dbfe4e57c98e3853b59ab3d42d1e
danil <danil@DESKTOP-O5TOTBU>
2024-04-14 14:37:29 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float one, two, result;
+            char sign;
+            Console.WriteLine("Введите первое число, а затем знак действия (только +)");
+            Console.Write("Первое число - ");
+            one = Convert.ToSingle(Console.ReadLine());
+            Console.Write("Введите знак действия : ");
+            sign = Convert.ToChar(Console.ReadLine());
+            Console.Write("Второе число - ");
+            two = Convert.ToSingle(Console.ReadLine());
+            if (sign == '+')
+            {
+                result = one + two;
+                Console.WriteLine("Сумма ваших чисел равна " + result);
+                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+            }
+            
+
+        }
+
+    }
+}
