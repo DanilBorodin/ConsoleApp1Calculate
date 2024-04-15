58abff08b479e60517ba91fa8a91e8d9707871b4
danil <danil.borodin.98@mail.ru>
2024-04-15 22:21:43 +0700

    Ввел проверку второго числа, перевел на Deci,al

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,19 +4,22 @@
     {
         private static void Main(string[] args)
         {
-            float one, two, result;
+            decimal one, two, result;
             char sign;
             Console.WriteLine("Введите первое число, а затем знак действия (только +)");
             Console.Write("Первое число - ");
-            while (Single.TryParse(Console.ReadLine(), out one) == false)
+            while (Decimal.TryParse(Console.ReadLine(), out one) == false)
             {
                 Console.WriteLine("Введите корректное значение");
                 Console.Write("Первое число - ");
             }
-            Console.Write("Введите знак действия : ");
-            sign = Convert.ToChar(Console.ReadLine());
+            Console.Write("Введите знак действия: ");
             Console.Write("Второе число - ");
-            two = Convert.ToSingle(Console.ReadLine());
+            while (Decimal.TryParse(Console.ReadLine(), out two) == false)
+            {
+                Console.WriteLine("Введите корректное значение");
+                Console.Write("Второе число - ");
+            }
             if (sign == '+')
             {
                 result = one + two;
@@ -30,6 +33,11 @@
                 Console.WriteLine("Умножение ваших чисел равна " + result);
                 Console.WriteLine("Для выхода нажмите любую клавишу...");
                 Console.ReadKey();
+                if (sign != '+');
+                {
+                    Console.WriteLine("Введите корректный знак");
+                    Console.ReadKey();
+                }
             }
         }
     }
